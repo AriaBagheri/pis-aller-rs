a2085ef686ba3e723646bf22fdec4a7629c6c1d5
Aria Bagheri <ariab9342@gmail.com>
2025-02-22 04:15:09 +0000

    removed unnecessary standard error

--- src/record.rs
+++ src/record.rs
@@ -1,13 +1,11 @@
 use serde::Serialize;
 use crate::item::PisAller;
-use standard_error::traits::*;
 
-trait PisAllerTrait: Serialize + StandardErrorDescriptionTrait + StandardErrorCausesTrait + StandardErrorSolutionsTrait {}
 
 #[derive(Debug, Serialize, Clone)]
 pub struct PisAllerRecord {
     pub tag: &'static str,
-    pub error: dyn PisAllerTrait,
+    pub error: String,
     #[serde(flatten)]
     pub data: PisAller,
     pub retries: usize,
